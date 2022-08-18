6d77ccbcd6723934135141296db184d022c45d08
uhaseeb <uhaseeb731@gmail.com>
2022-08-18 11:04:56 +0500

    class based views

--- review/urls.py
+++ review/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ReviewView.as_view()),
-    path('thank-you', views.thankyou)
+    path('thank-you', views.ThankyouView.as_view()),
+    path('reviews', views.ReviewsListView.as_view()),
+    path('reviews/<int:pk>', views.SingleReviewView.as_view())
 ]
--- review/views.py
+++ review/views.py
@@ -3,21 +3,37 @@ from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, 'index.html', {'form': form})
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "index.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
 
-        return render(request, 'index.html', {'form': form})
+class ThankyouView(TemplateView):
+    template_name = 'thankyou.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works"
+        return context
 
 
-def thankyou(request):
-    return render(request, 'thankyou.html')
+class ReviewsListView(ListView):
+    template_name = 'reviews.html'
+    model = Review
+    context_object_name = "reviews"
+
+
+class SingleReviewView(DetailView):
+    template_name = 'single-review.html'
+    model = Review
+    context_object_name = 'review'
